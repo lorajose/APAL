02a33631858548c24aa729783371ce92eff6d933
Jose Eduardo Lora Vargas <jsloravargas@gmail.com>
2024-12-12 03:35:21 +0000

    APAL-Mobile Release One

--- force-app/main/default/lwc/flowLauncher/flowLauncher.js
+++ force-app/main/default/lwc/flowLauncher/flowLauncher.js
@@ -1,63 +1,148 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import getFlowsAndCounts from '@salesforce/apex/FlowLauncherController.getFlowsAndCounts';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent'; 
+//import { subscribe, unsubscribe, onError, setDebugFlag } from 'lightning/empApi';
 
 export default class FlowLauncher extends NavigationMixin(LightningElement) {
     @api recordId; // ID del caso actual
-    buttons = [];
-    isMobile = false;
+    @track buttons = [];
+    //isLoading = false;
+    //subscription = null;
+    //channelName = '/event/FlowStatusChange__e';
 
     connectedCallback() {
-        // Usar window.matchMedia para detectar si el dispositivo es móvil
-        this.checkIfMobile();
-        // Agregar un listener de resize para reaccionar a cambios de tamaño
-        window.addEventListener('resize', this.checkIfMobile.bind(this));
-    }
-
-    // Verificar si el dispositivo es móvil usando matchMedia
-    checkIfMobile() {
-        const mediaQuery = window.matchMedia('(max-width: 318px)');
-        this.isMobile = mediaQuery.matches; // Establece isMobile como true si la pantalla es pequeña
-        console.log(`Es móvil: ${this.isMobile}`);
-    }
+     //  this.refreshFlowsAndCounts();
+       //this.subscribeToPlatformEvent();
+    } 
 
     @wire(getFlowsAndCounts, { caseId: '$recordId' })
     wiredFlows({ error, data }) {
         if (data) {
-            this.buttons = data.map(item => ({
-                key: item.objectName,
-                label: `${item.objectLabel} (${item.recordCount})`,
-                flowApiName: item.flowApiName
-            }));
+            this.refreshFlowsAndCounts(data);
+            console.log('data inside wire ', data);
         } else if (error) {
             console.error('Error al cargar flujos:', error);
         }
     }
+
+   /*  disconnectedCallback() {
+       this.unsubscribeFromPlatformEvent();
+    } */
+
+     // Este método actualiza los botones con los nuevos datos
+     refreshFlowsAndCounts(data) {
+        this.buttons = data.map(item => ({
+            key: item.objectName,
+            label: `${item.objectLabel} (${item.recordCount})`,
+            flowApiName: item.flowApiName
+        }));
+    }
+
+    // Llamar al Apex Controller para obtener los datos
+    /* refreshFlowsAndCounts() {
+        // this.isLoading = true;
+         console.log('recordId ', this.recordId);
+         getFlowsAndCounts({ caseId: this.recordId })
+             .then((data) => {
+                 console.log('data -> ', data);
+                 if (!data || data.length === 0) {
+                     console.warn('No flows found.');
+                     this.buttons = [];
+                 }else {
+                     this.buttons = data.map(item => ({
+                         key: item.objectName,
+                         label: `${item.objectLabel} (${item.recordCount})`,
+                         flowApiName: item.flowApiName
+                     }));
+                     console.log('buttons => ', this.buttons);
+              }
+             })
+             .catch((error) => {
+                 console.error('Error fetching flows and counts:', error);
+             })
+             .finally(() => {
+                 this.isLoading = false;
+             });
+     } */
+
     handleButtonClick(event) {
         const flowApiName = event.target.dataset.flowName;
+        console.log('Flow Open Event:', flowApiName); 
+        const flowUrl = `/flow/${flowApiName}?recordId=${this.recordId}`;
         this[NavigationMixin.Navigate]({
             type: 'standard__webPage',
             attributes: {
-                url: `/flow/${flowApiName}?recordId=${this.recordId}`
+                url: flowUrl
             }
+        }
+      );
+    } 
+    
+    // Suscripción al Platform Event
+    /* subscribeToPlatformEvent() {
+        const messageCallback = (response) => {
+            console.log('Nuevo evento recibido:', response);
+            const message = response.data.payload;
+            this.handleFlowStatusChange(message);
+        };
+
+        subscribe(this.channelName, -1, messageCallback)
+            .then((response) => {
+                console.log('Suscripción exitosa:', response.channel);
+                this.subscription = response;
+            })
+            .catch((error) => {
+                console.error('Error al suscribirse:', error);
+            });
+
+        onError((error) => {
+            console.error('Error en Platform Event:', error);
         });
-    }
+    } */
 
-   // Computar el nombre de la clase para los botones
-   get computeButtonClass() {
-    const buttonClass = this.isMobile ? 'mobile-button' : 'desktop-button';
-    
-    // Mostrar el toast cuando cambie la clase
-    this.dispatchEvent(
-        new ShowToastEvent({
-            title: 'Clase de botón',
-            message: `La clase de botón es: ${buttonClass}`,
-            variant: 'info',
-        })
-    );
-    
-    return buttonClass;
-}
 
+   /* handleFlowStatusChange(message) {
+        console.log('Flow Status Change Event:', message);
+        console.log('Actualizando los botones...', message.FlowName__c);
+        this.refreshFlowsAndCounts();   // Mensaje de actualización de botones
+        const actionsToNavigateBack = ['Save', 'Cancel'];
+        alert('message.FlowName__c ' + message.FlowName__c);
+        if (actionsToNavigateBack.includes(message.FlowName__c)) {
+                this.navigateBackToRecord();
+               // Actualizar los botones después de volver al caso
+        }
+    } */
+
+  /*  navigateBackToRecord() {
+        console.log('Record ID:', this.recordId); // Asegúrate de que este valor sea correcto
+        // Navigate to a Salesforce record page
+        // this[NavigationMixin.Navigate]({
+        //     type: 'standard__recordPage',
+        //     attributes: {
+        //         recordId: this.recordId, // Use your actual record ID here
+        //         objectApiName: 'Case', // Specify the object (e.g., Account)
+        //         actionName: 'view' // Action to perform (e.g., view, edit)
+        //     }
+        // });
+        let caseRecordIdURL = `com.salesforce.fieldservice://v1/sObject/${this.recordId}/details`;
+        this[NavigationMixin.Navigate]({
+                type: 'standard__webPage',
+                attributes: {
+                    url: caseRecordIdURL
+                }
+            }
+        );
+    } */
+
+    // Desuscripción del Platform Event
+   /* unsubscribeFromPlatformEvent() {
+        if (this.subscription) {
+            unsubscribe(this.subscription, (response) => {
+                console.log('Desuscripción exitosa:', response);
+            })
+            .catch((error) => {
+                console.error('Error al desuscribirse:', error);
+            });
+        }
+    } */
 }
